e9d75ced7a90dd561f7f0fa1688e6c77ea699c95
Тещин Антон <anton.teshchin@outlook.com>
2019-04-03 13:53:06 +0500

    first fix

---
+++ TypesApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesApp1
+{
+    /// <summary>
+    /// Playing with Types
+    /// </summary>
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Testing t = new Testing();
+
+            Console.WriteLine("Who Test");
+            t.WhoTest();
+
+            Console.WriteLine("Back Test");
+            t.BackTest();
+
+            Console.WriteLine("OLoad Test");
+            t.OLoadTest();
+
+            Console.WriteLine("To String Test");
+            t.ToStringTest();
+
+            Console.WriteLine("From String Test");
+            t.FromStringTest();
+
+            Console.WriteLine("Check Uncheck Test");
+            t.CheckUncheckTest();
+        }
+    }
+}
---
+++ TypesApp1/Testing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesApp1
+{
+    class Testing
+    {
+        public void WhoTest()
+        {
+
+        }
+        public void BackTest()
+        {
+
+        }
+        public void OLoadTest()
+        {
+
+        }
+        public void ToStringTest()
+        {
+
+        }
+        public void FromStringTest()
+        {
+
+        }
+        public void CheckUncheckTest()
+        {
+
+        }
+    }
+}
